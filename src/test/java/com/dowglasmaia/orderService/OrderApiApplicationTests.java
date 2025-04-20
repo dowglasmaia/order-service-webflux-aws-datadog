78ee137b61b1b9d059857ce871a9f2b2534c9ba4
Dowglas Maia <dowglasmaia@gmail.com>
2025-04-20 16:10:51 -0300

    Create Structure

---
+++ src/test/java/com/dowglasmaia/orderService/OrderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dowglasmaia.orderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
